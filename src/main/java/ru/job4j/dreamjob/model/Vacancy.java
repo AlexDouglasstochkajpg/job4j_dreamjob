aa3512ff9ce479f3b9b218b2046f3f853a10690c
AlexDouglasstochkajpg <domnitzkij.alexander@yandex.ru>
2025-05-08 15:45:59 +0300

    4. Создание вакансии [#504849]

--- src/main/java/ru/job4j/dreamjob/model/Vacancy.java
+++ src/main/java/ru/job4j/dreamjob/model/Vacancy.java
@@ -13,11 +13,14 @@ public class Vacancy {
 
     LocalDateTime creationDate;
 
-    public Vacancy(int id, String title, String description) {
+    public Vacancy() {
+    }
+
+    public Vacancy(int id, String title, String description, LocalDateTime creationDate) {
         this.id = id;
         this.title = title;
         this.description = description;
-        this.creationDate = LocalDateTime.now();
+        this.creationDate = creationDate;
     }
 
     public int getId() {
