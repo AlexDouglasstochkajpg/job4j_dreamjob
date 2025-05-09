d2a7564d98d3e9bec7dca8b8a90133e50f21f346
AlexDouglasstochkajpg <domnitzkij.alexander@yandex.ru>
2025-05-09 13:21:12 +0300

    7. Создание, просмотр, редактирование и удаление кандидатов [#504858]

--- src/main/java/ru/job4j/dreamjob/model/Candidate.java
+++ src/main/java/ru/job4j/dreamjob/model/Candidate.java
@@ -6,9 +6,15 @@ import java.util.Objects;
 public class Candidate {
 
     private int id;
+
     private String name;
+
     private String description;
-    LocalDateTime creationDate;
+
+    private LocalDateTime creationDate = LocalDateTime.now();
+
+    public Candidate() {
+    }
 
     public Candidate(int id, String name, String description, LocalDateTime creationDate) {
         this.id = id;
